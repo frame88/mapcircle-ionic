1b9e1bb5f54b28636684fe5faeb3b125d0f9cf57
egidiosalinaro <egy.s96@gmail.com>
2024-09-20 05:46:58 +0000

    20 set 2024, 07:46

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,20 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { SupabaseService } from '../supabase.service';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.page.html',
+  template: `
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <div class="ion-padding">
+        <h1>Supabase + Ionic Angular</h1>
+        <p>Sign in via magic link with your email below</p>
+      </div>
+      <ion-list inset="true">
+        <form (ngSubmit)="handleLogin($event)">
+          <ion-item>
+            <ion-label position="stacked">Email</ion-label>
+            <ion-input
+              [(ngModel)]="email"
+              name="email"
+              autocomplete
+              type="email"
+            ></ion-input>
+          </ion-item>
+          <div class="ion-text-center">
+            <ion-button type="submit" fill="clear">Login</ion-button>
+          </div>
+        </form>
+      </ion-list>
+    </ion-content>
+  `,
   styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  email = '';
 
-  constructor() { }
+  constructor(private readonly supabase: SupabaseService) {}
 
-  ngOnInit() {
+  async handleLogin(event: any) {
+    event.preventDefault();
+    const loader = await this.supabase.createLoader();
+    await loader.present();
+    try {
+      const { error } = await this.supabase.signIn(this.email);
+      if (error) {
+        throw error;
+      }
+      await loader.dismiss();
+      await this.supabase.createNotice('Check your email for the login link!');
+    } catch (error: any) {
+      await loader.dismiss();
+      await this.supabase.createNotice(
+        error.error_description || error.message
+      );
+    }
   }
-
 }

3c2cea67032891b86cc26a33258d14374f9af3b8
egidiosalinaro <egy.s96@gmail.com>
2024-09-20 05:49:10 +0000

    20 set 2024, 07:49

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { Router } from '@angular/router';
+import { SupabaseService } from './supabase.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  template: `
+    <ion-app>
+      <ion-router-outlet></ion-router-outlet>
+    </ion-app>
+  `,
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private supabase: SupabaseService, private router: Router) {
+    this.supabase.authChanges((_, session) => {
+      console.log(session);
+      if (session?.user) {
+        this.router.navigate(['/account']);
+      }
+    });
+  }
 }

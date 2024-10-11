19098c5daaa016cd4bfd37a5a7992e202abfed67
egidiosalinaro <egy.s96@gmail.com>
2024-10-11 15:51:56 +0000

    11 ott 2024, 17:51

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AccountComponent
   ],
   imports: [
     BrowserModule

50d2502a48ff57ae164b50bc754d43114b078a13
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 18:05:43 +0000

    13 ott 2024, 20:05

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: [
+        '',
+        [Validators.required, Validators.email, Validators.minLength(5)],
+      ],
+      password: ['', [Validators.required, Validators.minLength(7)]],
+    });
+  }
 }

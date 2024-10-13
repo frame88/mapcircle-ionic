d7004c9db801a9b8f65c475fbfaa2b16ae68f675
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 18:21:00 +0000

    13 ott 2024, 20:21

--- src/app/service/supa.service.ts
+++ src/app/service/supa.service.ts
@@ -1,13 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupaService {
-  constructor(private http: HttpClient) {}
+  private supabase_client: SupabaseClient;
+  constructor() {
+    this.supabase_client = createClient(
+      environment.supabase.url,
+      environment.supabase.key
+    );
+  }
 
-  getData() {
-    return this.http.get('https://api.example.com/data');
+  //  register
+  signUp(email: string, password: string) {
+    return this.supabase_client.auth.signUp({ email, password });
+  }
+
+  //  login
+  signIn(email: string, password: string) {
+    return this.supabase_client.auth.signInWithPassword({ email, password });
   }
 }

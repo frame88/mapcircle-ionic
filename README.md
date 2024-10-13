c89a991fda4cb9dd6138dd99943742b1e87a65fe
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 16:38:33 +0000

    13 ott 2024, 18:38

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SupabaseAngular
+# Coupongo
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.9.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'supabase-angular'`, () => {
+  it(`should have as title 'coupongo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('supabase-angular');
+    expect(app.title).toEqual('coupongo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, supabase-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coupongo');
   });
 });

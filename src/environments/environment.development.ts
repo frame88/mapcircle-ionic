1c72fe5cfc75e42fb620fcd8782a5b1a84cf4d82
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 17:27:01 +0000

    13 ott 2024, 19:27

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  supabase: {},
+  supabase: {
+    url: 'https://jowbeedjcgemgrwlyrhn.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impvd2JlZWRqY2dlbWdyd2x5cmhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIyNzg5NDcsImV4cCI6MjAzNzg1NDk0N30.NEr7XI4yMlvrl97kvMHZDGA7_FO9YC7ptbl8nJg0xHo',
+  },
 };

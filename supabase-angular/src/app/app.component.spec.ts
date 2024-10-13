54dd7635b5b475a5a6b4264f67a56d3c5c9cf5e0
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 16:36:26 +0000

    13 ott 2024, 18:36

--- supabase-angular/src/app/app.component.spec.ts
+++ supabase-angular/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coupongo'`, () => {
+  it(`should have as title 'supabase-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coupongo');
+    expect(app.title).toEqual('supabase-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, coupongo');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, supabase-angular');
   });
 });

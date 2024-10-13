7364954262e53ad3c79811cd9caf34b3ad27c5cc
egidiosalinaro <egy.s96@gmail.com>
2024-10-13 17:30:51 +0000

    13 ott 2024, 19:30

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
+  imports: [ReactiveFormsModule],
 })
 export class RegisterComponent {
-
+  registerForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder);
 }
